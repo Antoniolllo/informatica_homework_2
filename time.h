ea57bda79a22dc255f1551f3d4f20be3f0bf5bba
Елизавета Антонио <elizaveta.antonio@mail.ru>
2025-05-21 20:45:15 +0300

    Для Лабы 1

---
+++ time.h
@@ -0,0 +1,10 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+// Переводит часы в минуты
+double hours_to_minutes(double hours);
+
+// Переводит часы в секунды
+double hours_to_seconds(double hours);
+
+#endif
